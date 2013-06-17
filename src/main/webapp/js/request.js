aed28ca688f84d70dc8b6b32a19accf2ceef0b77
Hedi Hedda <hedda.hedi@etu.unice.fr>
2013-06-17 11:21:53 +0200

    hidden divs while loading xhr

--- src/main/webapp/js/request.js
+++ src/main/webapp/js/request.js
@@ -2,16 +2,18 @@ var req = new XMLHttpRequest();
 req.open('GET', "http://localhost:8080/webapp/server", false);
 
 req.onreadystatechange = function() {
+
     if (req.readyState === 4 && req.status === 200) {
         var res = JSON.parse(req.responseText);
         root = res.struct;
         inaltered_Root = root;
         constraints = res.const;
-        console.log(constraints);
         ressourcesEl = res.resources;
-
     }
+
 };
+document.getElementById("load").className = "hidden";
+document.getElementsByTagName("body")[0].className = "visible";
 req.send(null);
 
   
